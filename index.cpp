4bbe7a5c3c8462d0cd7e2e61d96c8383a423cede
Joey Peterson <joey.peterson15@gmail.com>
2024-09-21 09:18:59 -0700

    extract audio

--- index.cpp
+++ index.cpp
@@ -3,16 +3,27 @@
 
 int main() {
     std::string url = "https://www.youtube.com/watch?v=ZhHKyfZX7Gk";
-    std::string command = "yt-dlp " + url;
+    std::string command = "yt-dlp " + url + " -x --audio-format wav --paths wav:/Users/joeypeterson/Desktop/youtube_tracks";
+    // try {
+    //     FILE *fp = popen(command.c_str(), "r");
+    //     if (fp == NULL) {
+    //         // Handle error
+    //         std::cout << "there was an error" << std::endl;
+    //     }
+    //     pclose(fp);
+    // } catch (...) {
+    //     // Handle exception
+    //     std::cout << "there was an exception" << std::endl;
+    // }
+
     try {
         int result = system(command.c_str());
         if (result != 0) {
             // Handle error
-            std::cout << "there was an error" << std::endl;
         }
     } catch (...) {
         // Handle exception
-        std::cout << "there was an exception" << std::endl;
     }
+
     return 0;
 }
